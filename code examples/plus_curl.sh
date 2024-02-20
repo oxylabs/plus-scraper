070d81d84954261cfa5fed606c6a3ff765a604c3
AugustOxy <129506779+AugustOxy@users.noreply.github.com>
2024-02-20 14:56:54 +0200

    Adding code examples/plus_curl.sh

---
+++ code examples/plus_curl.sh
@@ -0,0 +1,4 @@
+curl --user user:pass \
+'https://realtime.oxylabs.io/v1/queries' \
+-H "Content-Type: application/json" \
+-d '{"source": "universal_ecommerce", "url": "https://www.plus.nl/producten/pasta-rijst-internationale-keuken/pasta/spaghetti-linguine-tagliatelle"}'
